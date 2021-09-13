bafae56ef3d0864290eb2a2215b955d77546f39e
Diogo Tavares <diogo.t0001@gmail.com>
2021-09-12 22:19:41 -0300

    Customized admin - auto slug

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Author, Article
 
-admin.site.register(Author)
-admin.site.register(Article)
+# admin.site.register(Author)
+# admin.site.register(Article)
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name','image_url')
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title','slug','author_id')
+    prepopulated_fields = {'slug':('title',)}
--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
  
 
 class Author(models.Model):
-    name = models.CharField(max_length=20,)
+    name = models.CharField(max_length=20)
     image_url = models.URLField(null=False)
 
     def __str__(self):
