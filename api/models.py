c9d6a61870c15a90d6f77dea9ea3c0e6b55a0c76
Diogo Tavares <diogo.t0001@gmail.com>
2021-09-14 20:48:35 -0300

    update models and migration

--- api/models.py
+++ api/models.py
@@ -1,18 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
- 
+import uuid
 
 class Author(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid1, editable=False)
     name = models.CharField(max_length=20)
-    photo = models.ImageField(null=False,upload_to='images')
+    photo = models.ImageField(null=True, upload_to='images')
 
     def __str__(self):
         return self.name
  
 class Article(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid1, editable=False)
     title = models.CharField(max_length=50)
     slug = models.SlugField(max_length=50, unique=True)
+    category = models.CharField(max_length=50)
     sumary = models.TextField()
+    fist_paragraph = models.TextField()
     body = models.TextField()
     author_id = models.ForeignKey(Author, on_delete=models.CASCADE)
 
